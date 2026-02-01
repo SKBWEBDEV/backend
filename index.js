6153d0933235c9fa2f9e3a461596436c9f9bc56a
SKBWEBDEV <najmujshakib025@gmail.com>
2026-02-01 20:39:37 +0600

    okey

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const homeControlers = require('./controlers/homeControlers')
-const check = require('./middleware/middleWare')
+const express = require ('express')
 
 const app = express()
 
-app.use('/home',check,homeControlers)
+app.use(express.json())
 
-app.listen(5000,()=> {
-  console.log("server running")
+app.post('/ragistation',(req,res)=> {
+  let{username,email,password} = (req.body)
+  console.log(username,email,password);
+
+  let errors = []
+
+  if(!username){
+   errors.push({
+    TypeError: "username",
+    massage: "username requird",
+   })
+  }
+  if(!email){
+   errors.push({
+    TypeError: "email",
+    massage: "right email requird"
+   })
+  }
+  if(!password){
+   errors.push({
+    TypeError: "password",
+    massage: "right password requird"
+   })
+  }
+  
+  res.status(errors.length == 0 ? 201 : 404).send({
+    success : errors.length == 0 ? true : false,
+    massage : errors.length == 0 ? "ragistation done" : "ragistation fail",
+    errors : errors.length == 0 ? null : errors
+  })
+})
+
+app.listen(8000,()=> {
+  console.log('server running');
+  
 })
